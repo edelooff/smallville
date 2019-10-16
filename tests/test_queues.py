b034f5819cb1de64c850c32c007eff6ee120e3d5
Elmer de Looff <elmer.delooff@gmail.com>
2019-10-16 23:16:37 +0200

    Adds simple tests for emptying and updating queues.

--- tests/test_queues.py
+++ tests/test_queues.py
@@ -1,5 +1,7 @@
 """Test suite for the smallville.queues module."""
 
+import pytest
+
 
 def test_empty_queue_falsy(queue):
     """An empty queue is Boolean False."""
@@ -22,3 +24,19 @@ def test_single_item_push(queue):
     q = queue()
     q['a'] = 1
     assert next(iter(q)) == ('a', 1)
+
+
+def test_iteration_empties_queue(queue):
+    """Iterating over the queue empties it."""
+    q = queue({'a': 1})
+    assert list(q) == [('a', 1)]
+    assert list(q) == []
+    with pytest.raises(StopIteration):
+        next(iter(q))
+
+
+def test_initialize_and_decrease(queue):
+    """Initializing and then decreasing that value return updated value."""
+    q = queue({'a': 1})
+    q['a'] = 5
+    assert list(q) == [('a', 5)]
